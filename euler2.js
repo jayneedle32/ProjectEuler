aa764df8996ceaa2789224e01770d26d086a65e9
jayneedle32 <jayneedle32@yahoo.com>
2015-08-18 19:56:09 -0700

    Update euler2.js

--- euler2.js
+++ euler2.js
@@ -1,6 +1,7 @@
 
 var previous = 0;
 var current = 1;
+var sum = 0;
 var next;
 
    for(i = 1; i < 100; i++){
@@ -8,6 +9,7 @@ var next;
         previous = current;
         current = next; 
         if(current % 2 === 0 && current < 4000000) {
-        console.log(current);
+            sum += current;
+        console.log(sum);
         }
    }
