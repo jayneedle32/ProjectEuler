b17581cfdcaf15ff0065fe382e496bae7f890872
jayneedle32 <jayneedle32@yahoo.com>
2015-08-18 22:03:05 -0700

    Create euler3.js

---
+++ euler3.js
@@ -0,0 +1,14 @@
+/*Used java more like a calculator than anything. I am doing the Odin Project and do not //
+feel we have learned enough to create java code from scratch to obtain the answer.*/
+
+a = 1000;
+b = a / 2;
+console.log(b);
+c = b / 2;
+console.log(c);
+d = c / 2;
+console.log(d);
+e = d / 5;
+console.log(e);
+f = e / 5;
+console.log(f);
